99d7cf5fe74550e5c0a83ad934c3237587da3f2d
JollyFuzz <mazcoding@gmail.com>
2025-09-19 11:13:52 +0300

    оптимальное скользящее окно 209

--- leetcode/209.py
+++ leetcode/209.py
@@ -3,8 +3,12 @@
 # как только достигли пробуем сдвинуть и сжать слева, получили новое наименьшее окно и потеряли таргет, повторяем
 
 
+from typing import List
+
+
 class Solution(object):
-    def minSubArrayLen(self, target, nums):
+    # Решение через скользящее окно, но неоптимальная реализация скользящего
+    def minSubArrayLen_v1(self, target, nums):
         """
         :type target: int
         :type nums: List[int]
@@ -52,10 +56,47 @@ class Solution(object):
 
         return best_min_subarr_len if best_min_subarr_len is not None else 0
 
+    def minSubArrayLen(self, target, nums):
+        # левый указатель
+        left = 0
+        # Текущая сумма в скользящем окне
+        cur_sum = 0
 
+        best_len = len(nums) + 1
+ 
+        # right - правый указатель
+        for right in range(len(nums)):
+            cur_sum += nums[right]
 
+            if cur_sum >= target and best_len > right - left + 1:
+                 best_len = right - left + 1
+
+            while cur_sum >= target:
+                cur_sum -= nums[left]
+                left += 1
+                
+                if cur_sum >= target and best_len > right - left + 1:
+                    best_len = right - left + 1
+
+        return best_len if best_len <= len(nums) else 0
+    
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        right = 0
+        sumOfCurrentWindow = 0
+        res = float('inf')
+
+        for right in range(0, len(nums)):
+            sumOfCurrentWindow += nums[right]
+
+            while sumOfCurrentWindow >= target:
+                res = min(res, right - left + 1)
+                sumOfCurrentWindow -= nums[left]
+                left += 1
+
+        return res if res != float('inf') else 0
 # во всех циклах перепроврить точки выхода и выход за пределы массива
 
-# print(Solution().minSubArrayLen(11, [1,1,1,1,1,1,1,1]))
+print(Solution().minSubArrayLen(11, [1,1,1,1,1,1,1,1]))
 print(Solution().minSubArrayLen(7, [8]))
-# print(Solution().minSubArrayLen(7, [2,3,1,2,4,3]))
+print(Solution().minSubArrayLen(7, [2,3,1,2,4,3]))
